0685de4f35ce3123fcaa8fee62712d305e2e2991
qwasr <alanzenezi@gmail.com>
2025-10-17 01:23:12 -0300

    Update CartasSuperTrunfo.c
    Inserção total do código já testado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,65 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  #include <stdio.h>
+
+  typedef struct{
+      char state;     // Estado
+      char code[4];   // Código
+      char name[30];  // Nome
+      int pop;        // População
+      float area;     // Area em km^2
+      float gdp;      // PIB
+      int tours;      // Pontos turísticos
+  } card;
+  
   // Área para entrada de dados
+
+  card states[2];
+  char num[3];
+
+  for (int i=0; i<2; i++) {
+      printf("--- Carta %d ---\n", i+1);
+      
+      printf("Estado (A-H): ");
+      scanf(" %c", &states[i].state);
+
+      printf("Código da Carta (ex: 01): ");
+      scanf("%2s", num);
+      sprintf(states[i].code, "%c%s", states[i].state, num);
+
+      printf("Nome da Cidade: ");
+      getchar(); // Limpar buffer
+      fgets(states[i].name, sizeof(states[i].name), stdin);
+
+      printf("População: ");
+      scanf("%d", &states[i].pop);
+
+      printf("Área (km²): ");
+      scanf("%f", &states[i].area);
+
+      printf("PIB: ");
+      scanf("%f", &states[i].gdp);
+
+      printf("Número de Pontos Turísticos: ");
+      scanf("%d", &states[i].tours);
+
+      printf("\n");
+  }
 
   // Área para exibição dos dados da cidade
 
-return 0;
+  for (int i=0; i<2; i++) {
+      printf("--- Dados da Carta %d ---\n", i+1);
+      printf("Estado: %c\n", states[i].state);
+      printf("Código: %s\n", states[i].code);
+      printf("Cidade: %s", states[i].name);
+      printf("População: %d\n", states[i].pop);
+      printf("Área: %.2f km²\n", states[i].area);
+      printf("PIB: %.2f\n", states[i].gdp);
+      printf("Pontos Turísticos: %d\n", states[i].tours);
+      printf("\n");
+  }
+  
+  return 0;
 } 

b84feda3c284ac5a92f9e15da90b9e23454a4eb9
qwasr <alanzenezi@gmail.com>
2025-10-17 11:55:35 -0300

    Desafio Nível Mestre - Tema 2 - Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,22 +10,17 @@ int main() {
   #include <stdio.h>
 
   typedef struct{
-      char state;     // Estado
-      char code[4];   // Código
-      char name[30];  // Nome
-      int pop;        // População
-      float area;     // Area em km^2
-      float gdp;      // PIB
-      int tours;      // Pontos turísticos
+      char state;         // Estado
+      char code[4];       // Código
+      char name[30];      // Nome
+      int pop;            // População
+      float area;         // Area em km^2
+      float gdp;          // PIB
+      int tours;          // Pontos turísticos
+      float density;      // Densidade populacional
+      float gdpcapita;    // PIB per capita
+      float power;        // Super Poder
   } card;
-
-  float density(int pop, float area) {
-      return pop * area;
-  }
-  
-  float gdpcapita(int pop, float gdp) {
-      return gdp / pop;
-  }
   
   // Área para entrada de dados
 
@@ -37,29 +32,39 @@ int main() {
       
       printf("Estado (A-H): ");
       scanf(" %c", &states[i].state);
-
+  
       printf("Código da Carta (ex: 01): ");
       scanf("%2s", num);
       sprintf(states[i].code, "%c%s", states[i].state, num);
-
+  
       printf("Nome da Cidade: ");
       getchar(); // Limpar buffer
       fgets(states[i].name, sizeof(states[i].name), stdin);
-
+  
       printf("População: ");
       scanf("%d", &states[i].pop);
-
+  
       printf("Área (km²): ");
       scanf("%f", &states[i].area);
-
+  
       printf("PIB: ");
       scanf("%f", &states[i].gdp);
-
+  
       printf("Número de Pontos Turísticos: ");
       scanf("%d", &states[i].tours);
-
+  
+      states[i].density = states[i].pop * states[i].area;
+      states[i].gdpcapita = states[i].pop * states[i].gdp;
+  
+      states[i].power = states[i].pop + 
+          states[i].area + 
+          states[i].gdp + 
+          states[i].tours + 
+          states[i].gdpcapita -
+          states[i].density;
+  
       printf("\n");
-  }
+}
 
   // Área para exibição dos dados da cidade
 
@@ -67,16 +72,47 @@ int main() {
       printf("--- Dados da Carta %d ---\n", i+1);
       printf("Estado: %c\n", states[i].state);
       printf("Código: %s\n", states[i].code);
-      printf("Cidade: %s", states[i].name);
+      printf("Cidade: %s", states[i].name); // já inclui \n
       printf("População: %d\n", states[i].pop);
       printf("Área: %.2f km²\n", states[i].area);
       printf("PIB: %.2f\n", states[i].gdp);
-      printf("Pontos Turísticos: %d\n", states[i].tours);
-      printf("Densidade: %.2f\n", density(states[i].pop, states[i].area));
-      printf("PIB per Capita: %.2f\n", gdpcapita(states[i].pop, states[i].gdp));
+      printf("Pontos Turísticos: %d\n\n", states[i].tours);
+      printf("Super poder: %.2f\n", states[i].power);
       printf(".........................\n");
       printf("\n");
   }
+
+  // Comparação das cartas
+
+  printf("Comparação de Cartas:\n\n");
+
+  printf("População: Carta %s venceu (%d)\n",
+         states[0].pop > states[1].pop ? states[0].code : states[1].code,
+         states[0].pop > states[1].pop ? states[0].pop : states[1].pop);
+
+  printf("Área: Carta %s venceu (%.2f)\n",
+         states[0].area > states[1].area ? states[0].code : states[1].code,
+         states[0].area > states[1].area ? states[0].area : states[1].area);
+
+  printf("PIB: Carta %s venceu (%.2f)\n",
+         states[0].gdp > states[1].gdp ? states[0].code : states[1].code,
+         states[0].gdp > states[1].gdp ? states[0].gdp : states[1].gdp);
+
+  printf("Pontos Turísticos: Carta %s venceu (%d)\n",
+         states[0].tours > states[1].tours ? states[0].code : states[1].code,
+         states[0].tours > states[1].tours ? states[0].tours : states[1].tours);
+
+  printf("Densidade Populacional: Carta %s venceu (%.2f)\n",
+         states[0].density < states[1].density ? states[0].code : states[1].code,
+         states[0].density < states[1].density ? states[0].density : states[1].density);
+
+  printf("PIB per Capita: Carta %s venceu (%.2f)\n",
+         states[0].gdpcapita > states[1].gdpcapita ? states[0].code : states[1].code,
+         states[0].gdpcapita > states[1].gdpcapita ? states[0].gdpcapita : states[1].gdpcapita);
+
+  printf("Super Poder: Carta %s venceu (%.2f)\n",
+         states[0].power > states[1].power ? states[0].code : states[1].code,
+         states[0].power > states[1].power ? states[0].power : states[1].power);
   
   return 0;
 } 
